ce1dd714a84002408eb466eaad251bd67ac68ce3
unknown <johnatanpaes@gmail.com>
2023-09-03 15:39:05 -0300

    intensivão JS

---
+++ HS  Shop/main.js
@@ -0,0 +1,13 @@
+// const nomeProduto = "Bag Cinza HS";
+// const marca = "HS";
+// const preco = 60;
+// const nomeArquivoImg = "1bag-bone-cinza";
+
+// quando tenho varios valores associados a uma mesma váriaável, isso é chamado objeto. è descrito como abaixo.
+const produto1 = {
+    nome: 'Bag cinza HS',
+    marca: "HS",
+    preco: 70,
+    
+
+}
